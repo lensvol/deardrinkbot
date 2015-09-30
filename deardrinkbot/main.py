67eddde5a3b852421dd8f5f073cb4e28027794fa
Kirill Borisov <lensvol@gmail.com>
2015-09-30 10:46:08 +0300

    Обрабатываем эмодзи в ответах.

--- deardrinkbot/main.py
+++ deardrinkbot/main.py
@@ -2,30 +2,28 @@
 
 from bottle import request, route, run
 import os
+import re
 import random
 import telegram
+import yaml
 
 
-REPLIES = {
-    u'привет': [
-        u'Привет!',
-        u'Здравствуй!',
-        u'Рада тебя слышать. :)',
-        u'О! Кого я вижу :) Как у тебя дела?',
-    ],
-    u'пока': [
-        u'До встречи. :)',
-        u'Ты пиши, если что. :)',
-        u'Успехов! :)',
-        u'Всегда приятно тебя читать. :)',
-    ],
-}
+# Загружаем ответы из внешнего файла, если такой указан и существует
+REPLIES = {}
+replies_fn = os.environ.get('REPLIES_YAML', 'replies.yaml')
+if os.path.exists(replies_fn):
+    with open(replies_fn, 'r') as fp:
+        REPLIES = yaml.safe_load(fp.read())
 
-
+# Инициализируем бота
 bot_token = os.environ.get('BOT_TOKEN', None)
 if not bot_token:
     raise ValueError("No token found!")
 bot = telegram.Bot(token=bot_token)
+
+def replace_emoji_code(m):
+    code = m.group(1)
+    return getattr(telegram.Emoji, code.upper(), code)
 
 
 @route('/bot', method='POST')
@@ -42,9 +40,13 @@ def webhook_handler():
             message.encode('utf-8'),
         )
 
-        reply = REPLIES.get(message, 'Дорогая, выпей вина! ' + telegram.Emoji.WINE_GLASS)
+        reply = REPLIES.get(
+            unicode(message),
+            u'Дорогая, выпей вина! :wine_glass:',
+        )
         if isinstance(reply, list):
             reply = random.choice(reply)
+        reply = re.sub(':([a-z_]+):', replace_emoji_code, reply.encode('utf-8'))
 
         bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.TYPING)
         bot.sendMessage(
